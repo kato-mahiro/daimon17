55fe2261865c732253ce6e5befc8d5b2bdbb6c61
katomasahiro10 <katomasahiro10@gmail.com>
2019-06-21 14:21:18 +0900

    rename
    answer_vector -> correct_answer_vector

--- src/main.py
+++ src/main.py
@@ -17,13 +17,15 @@ class Wcst:
         self.current_rule = random.choice(self.rules)
         self.question_vector = []
         self.feedback_vector = []
-        self.answer_vector = []
+        self.correct_answer_vector = []
     def question(self):
         l = [0,1,2,3]
         color = [0,0,0,0]
         shape = [0,0,0,0]
         number = [0,0,0,0]
+        self.correct_answer_vector = [0,0,0,0]
         correct_answer_number = l.pop(random.randint(0,len(l)-1))
+        self.correct_answer_vector[correct_answer_number] = 1
         if self.current_rule == 'color':
             color[correct_answer_number] = 1
             shape[l.pop(random.randint(0,len(l)-1))] = 1
@@ -43,8 +45,8 @@ class Wcst:
         self.question_vector += [0,0]
         self.question_vector += [0,0,0,0]
         return(self.question_vector)
-    def feedback(self):
-        pass
+    def feedback(self,result):
+        self.feedback_vector = self.question_vector[:12]
     def change_rule(self):
         next_rule = random.choice(self.rules)
         while(next_rule == self.current_rule):
