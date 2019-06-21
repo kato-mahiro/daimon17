23afc89cf37ee6827544c1f4dc01280eb63cde77
katomasahiro10 <katomasahiro10@gmail.com>
2019-06-21 13:10:27 +0900

    edit Wcst class
    implement change_rule func

--- src/main.py
+++ src/main.py
@@ -19,38 +19,39 @@ class Wcst:
         self.feedback_vector = []
         self.answer_vector = []
     def question(self):
-        print(self.current_rule)
         l = [0,1,2,3]
         color = [0,0,0,0]
         shape = [0,0,0,0]
         number = [0,0,0,0]
-        correct_answer = l.pop(random.randint(0,len(l)-1))
-        print(correct_answer)
+        correct_answer_number = l.pop(random.randint(0,len(l)-1))
         if self.current_rule == 'color':
-            color[correct_answer] = 1
+            color[correct_answer_number] = 1
             shape[l.pop(random.randint(0,len(l)-1))] = 1
             number[l.pop(random.randint(0,len(l)-1))] = 1
         elif self.current_rule == 'shape':
             color[l.pop(random.randint(0,len(l)-1))] = 1
-            shape[correct_answer] = 1
+            shape[correct_answer_number] = 1
             number[l.pop(random.randint(0,len(l)-1))] = 1
         elif self.current_rule == 'number':
             color[l.pop(random.randint(0,len(l)-1))] = 1
             shape[l.pop(random.randint(0,len(l)-1))] = 1
-            number[correct_answer] = 1
+            number[correct_answer_number] = 1
         self.question_vector += color
         self.question_vector += shape
         self.question_vector += number
         self.question_vector += [1,0]
         self.question_vector += [0,0]
         self.question_vector += [0,0,0,0]
-        print(self.question_vector)
-
+        return(self.question_vector)
     def feedback(self):
         pass
     def change_rule(self):
-        pass
+        next_rule = random.choice(self.rules)
+        while(next_rule == self.current_rule):
+            next_rule = random.choice(self.rules)
+        self.current_rule = next_rule
 
 if __name__=='__main__':
     wcst = Wcst()
     wcst.question()
+    wcst.change_rule()
